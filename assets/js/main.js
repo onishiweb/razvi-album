88d324bcf5c77d92332e6326ab9f71e5594f7881
Adam Onishi <onishiweb@gmail.com>
2018-03-11 23:11:29 +0000

    Remove lightbox custom template

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,21 +36,7 @@ function initFancybox() {
 		baseClass : 'ao-fancybox-base',
 
 		// Try to focus on the first focusable element after opening
-		autoFocus : true,
-
-		// Base template for layout
-		baseTpl :
-			'<div class="fancybox-container" role="dialog" tabindex="-1">' +
-				'<div class="fancybox-bg"></div>' +
-				'<div class="fancybox-inner">' +
-					'<div class="fancybox-infobar"></div>' +
-					'<div class="fancybox-toolbar">{{buttons}}</div>' +
-					'<div class="fancybox-navigation">{{arrows}}</div>' +
-					'<div class="fancybox-stage">' +
-						'<div class="fancybox-caption-wrap"><div class="fancybox-caption"></div></div>' +
-					'</div>' +
-				'</div>' +
-			'</div>'
+		autoFocus : true
 
 	};
 
